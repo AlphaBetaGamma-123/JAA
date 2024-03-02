4773cda840a5d63f0fb935f75f5a56eb9a9fbb8e
Amey Chande <YOUR_EMAIL>
2024-03-02 23:54:38 +1030

    ans to main35

---
+++ main-3-5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+double sum_even(double array[], int n); 
+
+int main () {
+
+    int array[] = {1, 2, 3, 4, 5};
+
+     std::cout << "Sum of elements at even positions: " << sum_even(array, size) << std::endl;
+
+    return 0;
+}
