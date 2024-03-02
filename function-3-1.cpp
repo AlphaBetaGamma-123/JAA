305423413ed73f04de3e7979ae5e9ab997f39748
Amey Chande <YOUR_EMAIL>
2024-03-02 23:46:08 +1030

    ans to function31

---
+++ function-3-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+bool is_fanarray(int array[], int n) {
+
+    if (n<1) {
+        return false; 
+    }
+    
+    int i;
+    for (i=0; i<=n/2; ++i ) {
+        if (array[i] != array[n-1-i] || array[i] != i+1 ) {
+            return false; 
+        } 
+    }
+    return true;
+}
