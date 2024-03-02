4eeb0fc6d290e0c2a9574022a43d1bfe4aba524e
Amey Chande <YOUR_EMAIL>
2024-03-02 23:54:07 +1030

    ans to main34

---
+++ main-3-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+void print_pass_fail(char grade);
+
+int main() {
+    char grade = 'B';
+    print_pass_fail(grade);
+
+    grade = 'D';
+    print_pass_fail(grade);
+
+    grade = 'C';
+    print_pass_fail(grade);
+
+    return 0;
+}
