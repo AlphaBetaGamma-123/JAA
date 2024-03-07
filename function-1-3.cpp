706d571760ae86ae33fda2953c3cb21979854ec8
Amey Chande <YOUR_EMAIL>
2024-03-08 01:23:17 +1030

    question2 easy

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,48 +1,62 @@
 #include <iostream>
 
-void count_digits(int array[4][4]){
+void count_digits(int array[4][4]) {
 
-    int counts[10] = {}; 
+    int zeroCount = 0, oneCount = 0, twoCount = 0, threeCount = 0, fourCount = 0, fifveCount = 0, sixCount = 0, sevenCount  = 0, eightCount = 0, nineCount = 0;
+    int row;
+    int column;
 
+    for (row = 0; row < 4; row++) {
+        for (column = 0; column < 4; column++) {
 
-
-     for (int i = 0; i<4; i++) { 
-
-
-        for (int j = 0; j<4; j++) {
-            
-             // Increment the count of the digit at array[i][j].
-            switch (array[i][j]) {
+            switch (array[row][column]) {
                 case 0:
+                zeroCount++; 
+                break; 
                 case 1:
+                oneCount++; 
+                break;
                 case 2:
-                case 3:
+                twoCount++;
+                break;
+                        case 3:
+                    threeCount++;
+                    break;
                 case 4:
+                    fourCount++;
+                    break;
                 case 5:
+                    fifveCount++;
+                    break;
                 case 6:
+                    sixCount++;
+                    break;
                 case 7:
+                    sevenCount++;
+                    break;
                 case 8:
+                    eightCount++;
+                    break;
                 case 9:
-                    counts[array[i][j]]++;
+                    nineCount++;
                     break;
                 default:
-                    // Ignore if the element is not a digit from 0 to 9.
+                    // Handle any other cases here
                     break;
+
+            }
 
         }
     }
-
-      // Printing out. 
-    for (int digit = 0; digit < 10; digit++) {
-        std:: cout << digit << ":" << counts[digit] << ";"; 
- 
-    }
-    std:: cout << std:: endl; 
-
-    }
-
-
+      
+      // Printing the count. 
+      std::cout << "0:" << zeroCount << ";1:" << oneCount << ";2:" << twoCount << ";3:" << threeCount << ";4:" << fourCount
+      << ";5:" << fifveCount << ";6:" << sixCount << ";7:" << sevenCount << ";8:" << eightCount << ";9:" << nineCount << ";" << std::endl;
 
 
 
 }
+
+
+
+
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -5,14 +5,14 @@ void count_digits(int array[4][4]);
 int main() {
 
 
-    int array[4][4] = {
+    int arrayTest1[4][4] = {
         {0, 1, 2, 3},
         {4, 5, 6, 7},
         {8, 9, 0, 1},
         {2, 3, 4, 5}
     };
 
-    count_digits(array);
+    count_digits(arrayTest1);
 
     return 0;
 }
