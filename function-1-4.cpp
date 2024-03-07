6dfb177399fcd3e3abf223324bd83e0ca4f6aa82
Amey Chande <YOUR_EMAIL>
2024-03-07 23:54:32 +1030

    easy question4

---
+++ function-1-4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void print_scaled(int array[3][3],int scale) {
+
+int value; 
+    for (int i = 0; i<3; i++) {
+        for (int j = 0; j<3; j++) {
+
+            value = array[i][j]*3;
+
+            // Printing the value.
+            std::cout << value << " ";
+        }
+        // Printing new line after each row. 
+        std::cout << std::endl;
+    }
+
+
+}
+
