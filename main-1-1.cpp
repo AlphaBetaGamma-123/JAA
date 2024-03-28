f74f5e46bed7854c5b7f97dc118fc9e67d420ed3
Amey Chande <YOUR_EMAIL>
2024-03-28 13:39:30 +1030

    q1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include <string>
+// #include <string>
 #include "MusicBox.h"
 
 
 
 int main () {
+    
 
-    MusicBox m1 ("hello", 3);
-    std:: cout << "Song name " << m1.get_song() << "Song width " << m1.get_width() << std::endl;
+    MusicBox box1; 
+    std:: cout << "Song name " << box1.get_song() << "Song width " << box1.get_width() << std::endl;
+
+    MusicBox box2(" Hello ", 4); 
+    std:: cout << "Song name " << box2.get_song() << "Song width " << box2.get_width() << std::endl; 
+
+
     return 0; 
 }
