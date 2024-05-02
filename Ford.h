1f2e2ea11ab722f0119c11dea80174ad0609c275
Amey Chande <YOUR_EMAIL>
2024-05-02 13:19:25 +0930

    q2

---
+++ Ford.h
@@ -0,0 +1,26 @@
+// Ford.h
+#ifndef FORD_H
+#define FORD_H
+
+#include "Car.h"
+
+class Ford : public Car {
+public:
+    Ford();
+    Ford(int badgeNumber, int price);
+
+    int get_badgeNumber() const;
+    void set_badgeNumber(int badgeNumber);
+
+    float get_litresOfFuel() const;
+    void set_litresOfFuel(float litres);
+
+    void refuel(int litres);
+    virtual void drive(int kms) override;
+
+private:
+    int badgeNumber;
+    float litresOfFuel;
+};
+
+#endif
