cd25d6961a17ff3310e487148767de0872d36f6d
Amey Chande <YOUR_EMAIL>
2024-05-15 18:25:27 +0930

    q2

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,13 +3,20 @@
 
 int main() {
     Tesla myTesla('S', 50000);
+
+    
     std::cout << "Tesla Model: " << myTesla.get_model() << std::endl;
 
 
-    std::cout << "Battery percentage: " << myTesla.get_batteryPercentage() << "%" << std::endl;
+
+    std::cout << "Battery percentage: " << myT
+    
+    esla.get_batteryPercentage() << "%" << std::endl;
 
     myTesla.drive(10);  
     std::cout << "Battery percentage after driving: " << myTesla.get_batteryPercentage() << "%" << std::endl;
+
+
 
 
     std::cout << "Emissions after driving: " << myTesla.get_emissions() << "g CO2" << std::endl;
